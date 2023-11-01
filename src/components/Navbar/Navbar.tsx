5cfc9fedc27c10b1d8fb849b2b058cfd5b3c6cf5
John Doe <andrewsimonmiller@gmail.com>
2023-11-01 22:24:53 +1100

    changed logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='navbar-container'>
         <div className='navbar-left-container'>
-        <Link className='logo-icon' href="/">≡|≡</Link>
+          <img className="rib-navbar-logo" src="https://res.cloudinary.com/dxvof8226/image/upload/v1698837362/RIB_HEART_PNG_4x_ipr8vl.png" />
         </div>
         <div className='navbar-right-container'>
           <ul className='navbar-right__links'>
